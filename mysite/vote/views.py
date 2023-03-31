0f477a8e363d3eb6952249f49daaf4fd9b0db288
Qwiple <qwiple51@gmail.com>
2023-03-31 16:20:28 +0500

    templates

--- mysite/vote/views.py
+++ mysite/vote/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
 
 def index(request):
@@ -14,9 +14,18 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-def results(requset, question_id):
-    response = 'You are looking at the results of question %s.'
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse('You voting at question %s' %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question':question, 'error_message': 'You didnt select a choice.', })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('vote:results', args=(question.id, )))
